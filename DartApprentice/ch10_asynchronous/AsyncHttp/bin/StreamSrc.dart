16af6b7ff9e8c4704c2ae2e706001ccd8222dd91
unclediga <zowie_i@mail.ru>
2022-03-10 19:32:56 +0300

    async-await + try-catch

--- DartApprentice/ch10_asynchronous/AsyncHttp/bin/StreamSrc.dart
+++ DartApprentice/ch10_asynchronous/AsyncHttp/bin/StreamSrc.dart
@@ -1,10 +1,16 @@
 import 'dart:io';
 
 Future<void> main() async {
-  final file = File('assets/lorem_long.txt');
-  final stream = file.openRead();
-  await for (var data in stream) {
-     print(data.length);
+  try{
+    final file = File('assets/lorem_long.txt');
+    final stream = file.openRead();
+    await for (var data in stream) {
+       print(data.length);
+    }
+  } on Exception catch(error){
+    print(error);
+  } finally {
+    print("All done!");     
   }
 }
   
