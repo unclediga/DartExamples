08cc3b4cd56d406f0a9b4f66e17f580460cf95e8
unclediga <zowie_i@mail.ru>
2022-03-10 18:32:10 +0300

    asyc-await http-query

---
+++ DartApprentice/ch10_asynchronous/AsyncHttp/bin/Main.dart
@@ -0,0 +1,66 @@
+import 'dart:convert';
+import 'dart:io';
+import 'package:http/http.dart' as http;
+
+class Todo{
+  Todo({
+    required this.userId,
+    required this.id,
+    required this.title,
+    required this.completed
+
+  });
+
+
+   factory Todo.fromJson(Map<String, Object?> jsonMap) {
+      return Todo(
+         userId: jsonMap['userId'] as int,
+         id: jsonMap['id'] as int,
+         title: jsonMap['title'] as String,         
+         completed: jsonMap['completed'] as bool,
+       );
+    } 
+
+  final int userId;
+  final int id;
+  final String title;
+  final bool completed;
+
+  String toString() {
+    return 'userId: $userId\n'
+           'id $id\n'
+           'title $title\n'
+           'compl $completed';
+          
+  }
+
+  
+}
+
+
+
+Future<void> main() async {
+
+
+  try {
+    final url = 'https://jsonplaceholder.typicode.com/todos/1';
+    final parsedUrl = Uri.parse(url);
+    final response = await http.get(parsedUrl);
+    final statusCode = response.statusCode;
+    if (statusCode == 200) {
+      final rawJsonString = response.body;
+      final jsonMap = jsonDecode(rawJsonString);
+      final todo = Todo.fromJson(jsonMap);
+      print(todo);
+    } else {
+      throw HttpException('$statusCode');
+    }
+  } on SocketException catch (error) {
+    print(error);
+  } on HttpException catch (error) {
+    print(error);
+  } on FormatException catch (error) {
+    print(error);
+  }
+}
+
